35d7ac68611bcc82039ebcf58cc77ecf085e063d
Nikolas Andreou <nickandreou94@gmail.com>
2023-04-21 17:08:17 +0300

    Allow usage of custom IDiscordClient implementations

--- Serilog.Sinks.Discord/DiscordSinkExtensions.cs
+++ Serilog.Sinks.Discord/DiscordSinkExtensions.cs
@@ -18,13 +18,15 @@ namespace Serilog.Sinks.Discord
         /// <param name="configurator">The sink's logging configuration delegate.</param>
         /// <param name="minimumLogLevel">The minimum level for events passed through the sink.</param>
         /// <param name="outputTemplate">A message template describing the format used to write to the sink. The default is <code>"{Message:lj}{NewLine}{NewLine}{Exception}"</code>.</param>
-        /// <param name="discordMessageProvider">Override this parameter to set a custom provider that creates a Discord Embed with your own styling.</param>
+        /// <param name="discordClient">The client which performs all communication with Discord's API. Override this to create your own Discord client that processes <see cref="LogEvent"/> differently.</param>
+        /// <param name="discordMessageProvider">The provider that transforms <see cref="LogEvent"/> to <see cref="Embed"/>. Override this parameter to set a custom provider that creates a Discord Embed with your own styling.</param>
         /// <returns></returns>
         public static LoggerConfiguration Discord(
             this LoggerSinkConfiguration config,
             LogEventLevel minimumLogLevel,
             Action<SinkConfiguration> configurator,
             string outputTemplate = "{Message:lj}{NewLine}{NewLine}{Exception}",
+            IDiscordClient discordClient = null,
             IValueProvider<Embed, LogEvent> discordMessageProvider = null)
         {
             configurator(SinkConfiguration);
@@ -34,9 +36,21 @@ namespace Serilog.Sinks.Discord
 
             ValidateConfiguration();
 
-            IValueProvider<Embed, LogEvent> messageProvider = discordMessageProvider ?? new DiscordMessageProvider(SinkConfiguration, ILogColorValueProvider.Default.Value);
+            if (discordClient != null && discordMessageProvider != null)
+            {
+                throw new DiscordSinkException(
+                    $"{nameof(discordMessageProvider)} can only be set when the default Discord client is used. " +
+                    $"Inject your custom {nameof(IValueProvider<Embed, LogEvent>)} in your {nameof(IDiscordClient)} implementation instead.");
+            }
             
-            DiscordClient client = new DiscordClient(SinkConfiguration, messageProvider, ILogLevelValueProvider.Default.Value);
+            IDiscordClient client = discordClient;
+
+            if (discordClient == null)
+            {
+                IValueProvider<Embed, LogEvent> messageProvider = discordMessageProvider ?? new DiscordMessageProvider(SinkConfiguration, ILogColorValueProvider.Default.Value);
+                client = new DiscordClient(SinkConfiguration, messageProvider, ILogLevelValueProvider.Default.Value);
+            }
+            
             return config.Sink(new DiscordSink(client, SinkConfiguration));
         }
 
